2133c41540ccf28de0551a9d7ee4c40ef1640d24
Shivam Singh <91836573+shivam16s@users.noreply.github.com>
2026-06-24 20:12:16 +0530

    [GFG] Sync: minimum-number-of-jumps-1587115620 (C++)

--- GFG/Medium/minimum-number-of-jumps-1587115620/solution.cpp
+++ GFG/Medium/minimum-number-of-jumps-1587115620/solution.cpp
@@ -1,20 +1,12 @@
-class Solution {
-  public:
-    int minJumps(vector<int>& arr) {
-        // code here
-          int n=arr.size();
-       vector<int>dp(n,INT_MAX);
-       dp[0]=0;
-       for(int i=0;i<n;i++){
-           if(dp[i]==INT_MAX)return -1;
-           dp[min(n-1,i+arr[i])]=min(dp[i]+1,dp[min(n-1,i+arr[i])]);
-           int j=min(n-1,i+arr[i])-1;
-           while(j>i && dp[j]==INT_MAX){
-               dp[j]=dp[min(n-1,i+arr[i])];
-               j--;
-           }
-       }
-       return dp[n-1];
-        
-    }
-};
+class Solution:
+	def minJumps(self, arr):
+	    # code here
+	    jump ,des, pos =0,0,0
+        for i in range(len(arr)-1):
+            des = max(des , i + arr[i])
+            if(i >= des):
+                return -1
+            if(pos == i):
+                pos = des
+                jump += 1
+        return jump
